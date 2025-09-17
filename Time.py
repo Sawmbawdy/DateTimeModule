41708655cb2f5e8795b21041ebb827872e3e5a33
Sawmbawdy <saiompatro12@gmail.com>
2025-09-17 19:43:14 -0400

    Update Time.py

--- Time.py
+++ Time.py
@@ -1,4 +1,7 @@
-import datetime as dt
-
-dt_now = dt.datetime.now()
-print(dt_now)
+from datetime import date, time, datetime
+import time as t
+dayte = date.today()
+datetimye = datetime.now()
+print("The current date is ", dayte)
+print("The current time is ", datetime.time(datetimye))
+print("The current date and time is ", datetimye)
